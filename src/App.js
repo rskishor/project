f02d785368eb26203a9743311d13a50e04e1fdb8
rskishor <arjunkishor33@gmail.com>
2024-06-24 20:29:54 +0530

    dfdf

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './login.css';
+import Profile from "./profile";
+const Login = () =>{
+  return(
+    <>
+<h3>R.S.KISHOR</h3>
+<div id="top">
+  
+  <a href="">HOME</a>
+  <a href="">ABOUT</a>
+  <a href="">SERVICES</a>
+  <a href="">PORTFOLIO</a>
+  <a href="">CONTACT</a>
+
+</div>
+<div id="name">
+<h4>HELLO, MY NAME IS</h4>
+
+<h1 id="gg">KISHOR</h1>
+<h2 id="hh">Web Developer</h2>
+
+
+</div>
+
+
+
+<Profile />
+    </>
+  );
+};
+
+export default Login;
---
+++ src/profile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './img.css'
+
+const Profile = () =>{
+    return(
+        <>
+        <div id ="img"></div>
+        
+        
+        
+        
+        
+        
+        
+        
+        </>
+    );
+};
+export default Profile;
