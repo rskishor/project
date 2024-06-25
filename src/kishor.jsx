3b63f99e6280d1d01988c4206a8468f2165abf7a
rskishor <arjunkishor33@gmail.com>
2024-06-25 20:38:14 +0530

    sdss

--- src/kishor.jsx
+++ src/kishor.jsx
@@ -1,28 +1,52 @@
 import React from "react";
+import './kis.css';
 const Ui = () =>{
 
 return(
     <>
     <div>
          <h2 id="ui">UI/UX DESIGN</h2>
-         <h1>hello</h1>
+         <h6 id="mn">An effective UI/UX is what<br></br>
+         captures attention and spreads a<br></br>
+         clear message.I make sure the<br></br>
+         design in innovative and neat<br></br>
+         with all of this.
+         </h6>
+         </div>
+    <div>
+        <h2 id="pop">WEB DEVELOPMENT</h2>
+        <h6 id="opp">If you are looking for a developer<br></br>
+            who’ll take over the research and<br></br>
+            development of your website, I<br></br>
+            am a well-established<br></br>
+            professional to help you with this.</h6>
+    </div>
 
-
-
+    <div>
+        <h2 id="even">APP DEVELOPMENT</h2>
+        <h6 id="null">If you are looking for a user-<br></br>
+        friendly app that will attract more<br></br>
+        mobile users, I can help you <br></br>
+        design and build a platform with <br></br>
+        the latest and trendiest look and<br></br>
+        feel.</h6>
     </div>
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+<div>
+    <div id="all">
+        <h1 id="skill">SKILL</h1>
+<div>
+    <h4>HTML</h4>
+    <div class="mark"><div class="my">80%</div></div>
+    <h4>CSS</h4>
+    <div class="mark"><div class="my">80%</div></div>
+    <h4>Javascript</h4>
+    <div class="mark"><div class="my">80%</div></div>
+    <h4>PYTHON</h4>
+    <div class="mark"><div class="my">80%</div></div>
+</div>
+</div>
+</div>
     </>
 
 
